7feac1368433d01947e7568fd028bdd020715508
Karim Metawea <ks.metawea@mohre.gov.ae>
2021-09-21 14:15:43 +0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Karem on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
